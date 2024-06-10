5eb4b196cbd53dfff5bde201394ec4ea8255569e
Jacob <jacobrashid404@gmail.com>
2024-06-10 16:09:41 -0700

    types

---
+++ types.ts
@@ -0,0 +1,14 @@
+// type to represent a show
+type tShow = {
+  id: number,
+  name: string,
+  summary: string,
+  image: string
+}
+
+// type to represent an episode
+type tEpisode = {
+  name: string,
+  season: number,
+  number: number
+}
