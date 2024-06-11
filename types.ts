f0e95ecd17fcb978f83869993a937290dfdc8c30
alicechang29 <147457604+alicechang29@users.noreply.github.com>
2024-06-10 17:49:22 -0700

    added types

--- types.ts
+++ types.ts
@@ -3,7 +3,7 @@ type tShow = {
   id: number,
   name: string,
   summary: string,
-  image: string
+  image: string | null
 }
 
 // type to represent an episode
@@ -14,3 +14,23 @@ type tEpisode = {
   number: number
 }
 
+// type to represent HTTP Methods
+type tHttpMethods = "get" | "post" | "patch" | "delete"
+
+// type to represent show result data
+type tShowResult = {
+  show: {
+    id: number,
+    name: string,
+    summary: string,
+    image: {medium: string} | null //image either has the key of medium or it will be null
+  }
+}
+
+// type to represent episode result data
+type tEpisodeResult = {
+  id: number,
+  name: string,
+  season: number,
+  number: number
+}
